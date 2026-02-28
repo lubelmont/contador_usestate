cfd89feb9846b973ec6473b2d613b05f588f20ec
Arnulfo Rodriguez <luis.rodriguez@lubelsoft.com>
2026-02-28 11:07:12 -0600

    add historial

--- App.js
+++ App.js
@@ -7,27 +7,51 @@ import ContadorBtn from './components/contadorBtn';
 export default function App() {
 
 
- const [contador, setContador] = useState(10);
+ const [contador, setContador] = useState(0);
+ const [historial, setHistorial] = useState([]);
 
+const getColor =() => {
+
+  if (contador > 0) {
+    return '#763de7';
+  } else if (contador < 0) {
+    return '#ef0d0d';
+  } else {
+    return '#ffffff';
+  }
+};
 
 
   return (
     <View style={styles.container}>
-      <Text style={styles.titulo}>Contador App</Text>
-      <Text style={styles.numeracion}> {contador} </Text>
+      <Text style={[styles.titulo, { color: getColor() }]}>Contador App</Text>
+      <Text style={[styles.numeracion, { color: getColor() }]}> {contador} </Text>
       <ContadorBtn 
         label="+ 1" 
         position="izquierda" 
-        onPressProp={() => setContador(contador + 1)} 
+        onPressProp={() => {
+          setContador(contador + 1);
+          setHistorial([...historial, contador + 1]);
+        }}
         onLongPressProp={() => setContador(0)}
         />
 
       <ContadorBtn 
         label="- 1" 
         position="derecha" 
-        onPressProp={() => setContador(contador - 1)} 
+        onPressProp={() => {
+          setContador(contador - 1);
+          setHistorial([...historial, contador - 1]);
+        }}
         onLongPressProp={() => setContador(0)}
         />
+
+        <Text style={[styles.tituloFotter, { color: getColor() }]}>Historial</Text>
+      
+          <Text style={[styles.historialItem, { color: getColor() }]}>
+            {historial.join(', ')}
+          </Text>
+  
 
       <StatusBar style="light" />
     </View>
@@ -43,7 +67,16 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 
-  titulo: {
+  tituloFotter: {
+    position: 'absolute',
+    bottom: 240,
+    fontSize: 32,
+    color: '#1e90ff',
+    fontWeight: 'bold',
+    letterSpacing: 2,
+  },
+
+    titulo: {
     position: 'absolute',
     top: 60,
     fontSize: 32,
@@ -54,7 +87,13 @@ const styles = StyleSheet.create({
 
   numeracion: {
     fontSize: 240,
-    color: '#ffffff',
+    fontWeight: 'bold',
+  },
+  historialItem: {
+    position: 'absolute',
+    bottom: 140,
+    fontSize: 20,
+    color: '#1e90ff',
     fontWeight: 'bold',
   },
 
